68d71f6406f2f9abb3b31264ae87eca2e11ebfba
vitorexfernandes <83621837+vitorexfernandes@users.noreply.github.com>
2023-08-18 22:49:46 +0100

    GetSkills Created

--- dungeonsanddragons/Controllers/CharacterController.cs
+++ dungeonsanddragons/Controllers/CharacterController.cs
@@ -12,6 +12,8 @@ public class CharacterController : ControllerBase
     private static List<CharacterClass> _classList = new List<CharacterClass>();
     private static List<CharacterSkill> _classskill = new List<CharacterSkill>();
 
+    //POST METHODS
+    //********************************************
     [HttpPost("CreateCharacter")]
     public void CreateCharacter([FromBody] Character character)
     {
@@ -34,10 +36,18 @@ public class CharacterController : ControllerBase
         foreach (var skill in skills)
         {
             _classskill.Add(skill);
-            Console.WriteLine(skill.Id);
+            Console.WriteLine("ID=" + skill.Id);
             Console.WriteLine(skill.Name);
-            Console.WriteLine(skill.Value);
+            Console.WriteLine("Valor=" + skill.Value);
+
         }
     }
 
+    //GET METHODS
+    //********************************************
+    [HttpGet("GetAllSkills")]
+    public List<CharacterSkill> GetAllSkills()
+    {
+        return _classskill;
+    }
 }
--- dungeonsanddragons/Models/CharacterSkill.cs
+++ dungeonsanddragons/Models/CharacterSkill.cs
@@ -1,12 +1,24 @@
-﻿namespace dungeonsanddragons.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace dungeonsanddragons.Models
 {
     public class CharacterSkill
     {
+        //Skill Atributes
         private static int _nextId = 1;
+
         public int Id { get; private set; }
+
+        [Required(ErrorMessage = "Skill Name is required")]
         public string Name { get; set; }
-        public int Value { get; set; } = 0;
+
         public string Description { get; set; }
+
+        [Required(ErrorMessage = "Skill Value is required")]
+        [Range(0, 10, ErrorMessage = "The skill value must be greater than or equal to 0 and less than or equal to 10")]
+        public int Value { get; set; }
+
+        
 
 
         public CharacterSkill(string name, string description, int value)
